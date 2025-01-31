89132b1f39facb26b9d376af418e34b6ab72486a
Hector PHARAM <pharamhector@gmail.com>
2025-01-31 18:18:26 +0100

    Cmake support + working blink example

---
+++ src/blinky.cpp
@@ -0,0 +1,47 @@
+
+#include <stm32f4xx_hal.h>
+
+// STM32F4-Discovery green led - PD12
+#define LED_PORT                GPIOG
+#define LED_PIN                 GPIO_PIN_14
+#define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOG_CLK_ENABLE
+
+//This prevent name mangling for functions used in C/assembly files.
+extern "C"
+{
+    void SysTick_Handler(void)
+    {
+        HAL_IncTick();
+
+        // 1 Hz blinking
+        if ((HAL_GetTick() % 500) == 0)
+        {
+            HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+        }
+    }
+}
+
+void initGPIO()
+{
+    GPIO_InitTypeDef GPIO_Config;
+
+    GPIO_Config.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_Config.Pull = GPIO_NOPULL;
+    GPIO_Config.Speed = GPIO_SPEED_FREQ_HIGH;
+
+    GPIO_Config.Pin = LED_PIN;
+
+    LED_PORT_CLK_ENABLE();
+    HAL_GPIO_Init(LED_PORT, &GPIO_Config);
+}
+
+int main(void)
+{
+    HAL_Init();
+    initGPIO();
+    // 1kHz ticks
+    HAL_SYSTICK_Config(SystemCoreClock / 1000);
+
+    while(1);
+    return 0;
+}
